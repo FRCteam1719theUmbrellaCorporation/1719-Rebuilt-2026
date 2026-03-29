a55388b654e323875bdfaadb87744e5e94179e43
matthewdgreen <matthewdgreen@users.noreply.github.com>
2026-03-29 09:05:34 -0400

    Blendervel to 150

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -136,7 +136,7 @@ public final class Constants
     public static final double DistancePowerMult = 0.0830589;
     public static final double DistancePowerOffset = 0.507221;
     public static final double ShooterScailTimeout = 2.d;
-    public static final double BloaderVel = 200;           
+    public static final double BloaderVel = 150;           
     public static final double PULSE_TIME = 2.0d;
     public static final double PULSE_BACK_TIME = 1.d;
   }
